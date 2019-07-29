3ecf36e21e5731041752a1a14dccd5a6295370d6
Blasco Paterno Castello <beukpaterno@hotmail.fr>
2019-07-29 12:04:35 +0200

    add routes and models

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -3,6 +3,7 @@ class Complaint < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :type
+  belongs_to :city
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
