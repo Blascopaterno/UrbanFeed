c540d50eb625e2b9de3db74a5059719a4ddfb8d8
Valentin Abeille <valentin.abeille@gmail.com>
2019-07-29 11:34:00 +0200

    Adding a mayor button and corresponding migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_154135) do
+ActiveRecord::Schema.define(version: 2019_07_29_085221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2019_07_24_154135) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "profession"
-    t.boolean "mayor", default: false
+    t.boolean "mayor"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
