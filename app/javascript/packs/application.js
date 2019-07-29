87386916e656cd8c03fa89e20880f165805efd77
Masha Novikava <mashanovikava@gmail.com>
2019-07-29 12:56:13 +0200

    the scroll button v1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,11 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { complaintBtns } from '../components/complaint_btns';
-import {initAutocomplete} from '../plugins/init_autocomplete'
+import {initAutocomplete} from '../plugins/init_autocomplete';
+
 
 initMapbox();
 initAutocomplete();
 
 complaintBtns();
+
