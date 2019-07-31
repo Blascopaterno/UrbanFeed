6537af949e88f77d874ec7c755cc82441183800e
Mariano Amoedo <zceimam@ucl.ac.uk>
2019-07-31 17:54:44 +0200

    form alignments

--- app/javascript/plugins/photo_uploader.js
+++ app/javascript/plugins/photo_uploader.js
@@ -2,12 +2,17 @@ const initPhotoUpload = () => {
   const input = document.getElementById("complaint_picture");
   const photoContainer = document.getElementById("attached");
   if (input) {
+      photoContainer.classList.add("invisible");
       input.addEventListener("change", (event) => {
       var reader = new FileReader();
       reader.onload = () => {
           photoContainer.src = reader.result;
+          photoContainer.classList.remove("invisible");
       }
-      reader.readAsDataURL(event.target.files[0]);
+      const file = event.target.files[0];
+      if (file) {
+        reader.readAsDataURL(file);
+      }
     });
   }
 }
